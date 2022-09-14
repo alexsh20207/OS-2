881e06da0a07f115c1ef144e0dcdc5f038c3a560
alexsh20207 <73633904+alexsh20207@users.noreply.github.com>
2022-09-14 18:53:14 +0700

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -18,12 +18,12 @@ int main() {
     int status;
     status = pthread_create(&thread, NULL, printMsg, NULL);
     if (status != SUCCESS) {
-        printf("pthread_create error:%d\n", status);
+        fprintf(stderr, "pthread_create error:%d\n", status);
         return PTHREAD_CREATE_ERR;
     }
     status = pthread_join(thread, NULL);
     if (status != SUCCESS) {
-        printf("pthread_join error:%d\n", status);
+        fprintf(stderr, "pthread_join error:%d\n", status);
         return PTHREAD_JOIN_ERR;
     }
     for (int i = 0; i < 10; ++i) {
