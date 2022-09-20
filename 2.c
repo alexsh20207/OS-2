5f6e22a1cb8f2fe1bb59f7daa5dd2e90bb2ebf24
alexsh20207 <73633904+alexsh20207@users.noreply.github.com>
2022-09-21 02:18:23 +0700

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define PTHREAD_CREATE_ERR -1
+#define PTHREAD_JOIN_ERR -2
+#define SUCCESS 0
+
+typedef struct argForThread {
+    const char* text;
+    int count;
+} argForThread;
+
+void* printText(void* p) {
+    argForThread* val = (argForThread*)p;
+    for (int i = 0; i < val->count; ++i) {
+        printf("%s %d\n", val->text, i);
+    }
+    return SUCCESS;
+}
+ 
+int main() {
+    pthread_t thread;
+    int status;
+    
+    argForThread mainArg = {"Main", 10};
+    argForThread newArg = {"Child", 10};
+    
+    status = pthread_create(&thread, NULL, printText, (void*)&newArg);
+    if (status != SUCCESS) {
+        fprintf(stderr,"pthread_create error:%d\n", status);
+        return PTHREAD_CREATE_ERR;
+    }
+    status = pthread_join(thread, NULL);
+    if (status != SUCCESS) {
+        fprintf(stderr, "pthread_join error:%d\n", status);
+        return PTHREAD_JOIN_ERR;
+    }
+    printText(&mainArg);
+    pthread_exit(NULL);
+}
